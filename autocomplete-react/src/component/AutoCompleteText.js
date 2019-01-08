61add2713a9c5082852c2cb7c489bf521c2e6e87
Stephan Griesel <sgriesel@gmail.com>
2019-01-08 06:11:37 +0100

    update textchanged

--- autocomplete-react/src/component/AutoCompleteText.js
+++ autocomplete-react/src/component/AutoCompleteText.js
@@ -9,11 +9,26 @@ export class AutoCompleteText extends React.Component {
             'Sannie',
             'Jannie'
         ];
+        this.state = {
+            suggestions: [],
+        };
     }
+
+    onTextChanged = (e) => {
+        const value = e.target.value;
+        let suggestions = [];
+        if (value.length > 0) {
+            const regex = new RegExp(`^${value}`, 'i');
+            suggestions = this.items.sort().filter(v => v.test(regex));
+        }
+        this.setState(() => ({ suggestions }));
+        }
+
+    
   render() {
     return (
       <div>
-        <input type="text" />
+        <input onChange={this.onTextChanged} type="text" />
         <ul>
             {this.items.map((item) => <li>{item}</li>)}
         </ul>
