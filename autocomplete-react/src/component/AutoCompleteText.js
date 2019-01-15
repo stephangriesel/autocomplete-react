23b63852b969a1095ad24ff75239f48ccdfd2611
Stephan Griesel <sgriesel@gmail.com>
2019-01-15 06:01:44 +0100

    update autocompletetext component

--- autocomplete-react/src/component/AutoCompleteText.js
+++ autocomplete-react/src/component/AutoCompleteText.js
@@ -1,40 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class AutoCompleteText extends React.Component {
-    constructor(props) {
-        super(props);
-        this.items = [
-            'Piet',
-            'Peester',
-            'Sannie',
-            'Jannie'
-        ];
-        this.state = {
-            suggestions: [],
-        };
+  constructor(props) {
+    super(props);
+    this.items = ["Piet", "Peester", "Sannie", "Jannie", "Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter"];
+    this.state = {
+      suggestions: []
+    };
+  }
+
+  onTextChanged = e => {
+    const value = e.target.value;
+    let suggestions = [];
+    if (value.length > 0) {
+      const regex = new RegExp(`^${value}`, "i");
+      suggestions = this.items.sort().filter(v => regex.test(v));
     }
+    this.setState(() => ({ suggestions }));
+  };
 
-    onTextChanged = (e) => {
-        const value = e.target.value;
-        let suggestions = [];
-        if (value.length > 0) {
-            const regex = new RegExp(`^${value}`, 'i');
-            suggestions = this.items.sort().filter(v => v.test(regex));
-        }
-        this.setState(() => ({ suggestions }));
-        }
+  renderSuggestions() {
+    const { suggestions } = this.state;
+    if (suggestions.length === 0) {
+      return null;
+    }
+    return (
+      <ul>
+        {this.items.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    );
+  }
 
-    
   render() {
     return (
       <div>
-        <input onChange={this.onTextChanged} type="text" />
-        <ul>
-            {this.items.map((item) => <li>{item}</li>)}
-        </ul>
+        <input value="" onChange={this.onTextChanged} type="text" />
+        {this.renderSuggestions()}
       </div>
-    )
+    );
   }
 }
 
-export default AutoCompleteText
+export default AutoCompleteText;
